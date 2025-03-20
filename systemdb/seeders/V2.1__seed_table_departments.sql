80f45c8cb89ab57a8773119c8737e3e8f5905753
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-03-21 02:04:33 +0700

    Cập nhật bảng chi nhánh

---
+++ systemdb/seeders/V2.1__seed_table_departments.sql
@@ -0,0 +1,20 @@
+-- Chèn dữ liệu cho phòng Kinh Doanh
+INSERT INTO "Department" (NAME, ADDRESS, DESCRIPTION, STATUS, CREATEDDATE, LASTMODIFIEDDATE)
+VALUES ('Phòng Kinh Doanh', 'Hà Nội', 'Chuyên phụ trách mảng kinh doanh', 'A', SYSTIMESTAMP, SYSTIMESTAMP);
+
+-- Chèn dữ liệu cho phòng Nhân Sự
+INSERT INTO "Department" (NAME, ADDRESS, DESCRIPTION, STATUS, CREATEDDATE, LASTMODIFIEDDATE)
+VALUES ('Phòng Nhân Sự', 'TP. Hồ Chí Minh', 'Quản lý nhân sự', 'A', SYSTIMESTAMP, SYSTIMESTAMP);
+
+-- Chèn dữ liệu cho phòng Kế Toán
+INSERT INTO "Department" (NAME, ADDRESS, DESCRIPTION, STATUS, CREATEDDATE, LASTMODIFIEDDATE)
+VALUES ('Phòng Kế Toán', 'Đà Nẵng', 'Xử lý các vấn đề tài chính', 'A', SYSTIMESTAMP, SYSTIMESTAMP);
+
+-- Chèn dữ liệu cho Chi Nhánh Hà Nội
+INSERT INTO "Department" (NAME, ADDRESS, DESCRIPTION, STATUS, CREATEDDATE, LASTMODIFIEDDATE)
+VALUES ('Chi Nhánh Hà Nội', 'Hà Nội', 'Chi nhánh Hà Nội', 'A', SYSTIMESTAMP, SYSTIMESTAMP);
+
+-- Chèn dữ liệu cho Chi Nhánh Sài Gòn
+INSERT INTO "Department" (NAME, ADDRESS, DESCRIPTION, STATUS, CREATEDDATE, LASTMODIFIEDDATE)
+VALUES ('Chi Nhánh Sài Gòn', 'TP. Hồ Chí Minh', 'Chi nhánh Sài Gòn', 'E', SYSTIMESTAMP, SYSTIMESTAMP);
+/
