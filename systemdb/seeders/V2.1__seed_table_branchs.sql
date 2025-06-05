6c6105d426aabc6c2b2144a53ffad20795a98fc7
Luyện Hải Đăng <106638147+luyenhaidangit@users.noreply.github.com>
2025-06-05 10:04:34 +0700

    Add missing branch seed data

---
+++ systemdb/seeders/V2.1__seed_table_branchs.sql
@@ -0,0 +1,7 @@
+-- Seed data for branch master
+INSERT INTO BRANCH_MASTER (CODE, NAME, ADDRESS)
+VALUES ('HN', 'Chi nhánh Hà Nội', 'Hà Nội');
+
+INSERT INTO BRANCH_MASTER (CODE, NAME, ADDRESS)
+VALUES ('HCM', 'Chi nhánh Hồ Chí Minh', 'TP. Hồ Chí Minh');
+/
