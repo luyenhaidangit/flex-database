150e3620b52069307af7d5c6d3a4e99358978cc3
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-06-29 17:54:57 +0700

    Sửa database

--- aspnetidentity/seeders/V1.1__seed_identity.sql
+++ aspnetidentity/seeders/V1.1__seed_identity.sql
@@ -1,5 +1,5 @@
-INSERT INTO "Users" ("Id", "UserName", "NormalizedUserName", "Email", "NormalizedEmail", "EmailConfirmed", "PasswordHash", "SecurityStamp", "ConcurrencyStamp", "PhoneNumber", "PhoneNumberConfirmed", "TwoFactorEnabled", "LockoutEnd", "LockoutEnabled", "AccessFailedCount") 
-VALUES (1, 'admin', 'ADMIN', 'luyenhaidangit@gmail.com', 'LUYENHAIDANGIT@GMAIL.COM', 1, 'AQAAAAIAAYagAAAAEOz4tb1nW6wxzqhXka7A7CniH/C5C2VUP+qR86SvqgDK1hfTHWtNo/w0WAOkY1Fm8Q==', 'NKFB7ZPVI6BUX5XEZ24RYZIYVFAUPI7G', '7a6d8be7-56fa-46a5-ba8f-6ba9ede03de2', null, 0, 0, null, 1, 0);
+INSERT INTO USERS (ID, USER_NAME, NORMALIZED_USER_NAME, EMAIL, NORMALIZED_EMAIL, EMAIL_CONFIRMED, PASSWORD_HASH, SECURITY_STAMP, CONCURRENCY_STAMP, PHONE_NUMBER, PHONE_NUMBER_CONFIRMED, TWO_FACTOR_ENABLED, MFA_SECRET, LOCKOUT_END, LOCKOUT_ENABLED, ACCESS_FAILED_COUNT, FULL_NAME, CREATED_AT, LAST_LOGIN, IS_ACTIVE, BRANCH_ID)
+VALUES (1, 'admin', 'ADMIN', 'luyenhaidangit@gmail.com', 'LUYENHAIDANGIT@GMAIL.COM', 1, 'AQAAAAIAAYagAAAAEOz4tb1nW6wxzqhXka7A7CniH/C5C2VUP+qR86SvqgDK1hfTHWtNo/w0WAOkY1Fm8Q==', 'NKFB7ZPVI6BUX5XEZ24RYZIYVFAUPI7G', '7a6d8be7-56fa-46a5-ba8f-6ba9ede03de2', NULL, 0, 0, NULL, NULL, 1, 0, 'Luyện Hải Đăng', SYSDATE, NULL, 1, 1);
 
 INSERT INTO "Roles" ("Id", "Name", "NormalizedName", "ConcurrencyStamp") 
 VALUES (1, 'Admin', 'ADMIN', 'ddf92f46-bd4b-4a4f-bf1f-4bce0d4b2f17');
