80a3dc76353b6f4098bb3581a7e89eb7c1831383
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-08-10 19:30:31 +0700

    Update

--- systemdb/seeders/V2.1__seed_table_branchs.sql
+++ systemdb/seeders/V2.1__seed_table_branchs.sql
@@ -1,4 +1,4 @@
 -- Seed data for branch master
 INSERT INTO FLEX_SYSTEM.BRANCHES (ID, CODE, NAME, BRANCH_TYPE, DESCRIPTION, IS_ACTIVE, STATUS, CREATED_BY, CREATED_DATE, UPDATED_BY, UPDATED_DATE) 
-VALUES (3, 'TCBQT', 'Techcombank Quang Trung', 1, 'Techcombank Quang Trung', '1', '1', 'admin', TIMESTAMP '2025-08-10 19:16:53', null, null);
+VALUES (3, 'TCBQT', 'Techcombank Quang Trung', 1, 'Techcombank Quang Trung', 'Y', '1', 'admin', TIMESTAMP '2025-08-10 19:16:53', null, null);
 /
